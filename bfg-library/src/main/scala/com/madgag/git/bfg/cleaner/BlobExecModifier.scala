5681643bf5ae80fb7c65b8d81796f030af7dbc3e
D German <dmg@uvic.ca>
2016-08-07 09:06:05 +0900

    it is working, it is tokenizing properly, but it is very hardcoded

--- bfg-library/src/main/scala/com/madgag/git/bfg/cleaner/BlobExecModifier.scala
+++ bfg-library/src/main/scala/com/madgag/git/bfg/cleaner/BlobExecModifier.scala
@@ -29,7 +29,7 @@ trait BlobExecModifier extends TreeBlobModifier {
 
     val bytes = ByteStreams.toByteArray(objectStream)
 
-    println(s"${entry.objectId.name}: 00 To execute : ${command} Length: ${bytes.length} on file ${entry.filename}")
+//    println(s"${entry.objectId.name}: 00 To execute : ${command} Length: ${bytes.length} on file ${entry.filename}")
 
     val newBytes = ArrayBuffer[Byte]()
 
@@ -56,16 +56,16 @@ trait BlobExecModifier extends TreeBlobModifier {
     val proc = pb.run(io)
     val exitCode = proc.exitValue
 
-    println(s"${entry.objectId.name}: 20 {$fileName} Waiting for BFG_BLOB Length: ${bytes.length} New bytes: ${newBytes.length} ExitCode ${exitCode}")
+//    println(s"${entry.objectId.name}: 20 {$fileName} Waiting for BFG_BLOB Length: ${bytes.length} New bytes: ${newBytes.length} ExitCode ${exitCode}")
 
     //    if(JavaArrays.equals(bytes, newBytes)) {
 
 
     if (exitCode != 0 || JavaArrays.equals(bytes, newBytes.toArray)) {
-      println(s"${entry.objectId.name}: 31 Nothing to do {$fileName}" )
+//      println(s"${entry.objectId.name}: 31 Nothing to do {$fileName}" )
       entry.withoutName
     } else {
-      println(s"${entry.objectId.name}: 35 to replace {$fileName}" )
+//      println(s"${entry.objectId.name}: 35 to replace {$fileName}" )
 
       val objectId = threadLocalObjectDBResources.inserter().insert(OBJ_BLOB, newBytes.toArray)
       entry.copy(objectId = objectId).withoutName
@@ -76,9 +76,9 @@ trait BlobExecModifier extends TreeBlobModifier {
   def fix(entry: TreeBlobEntry) = {
 
     val fileName = entry.filename.toString
-    val toProcess = "^(.+)\\.[ch]$".r
+    //    val toProcess = "^net(.+)\\.[ch]$".r
+    val toProcess = "(.+)\\.[ch]$".r
 
-    
     fileName match {
       case toProcess(_) => execute(entry)
       case _ => entry.withoutName
